7ee917df28eeeeeebe65f99df498668a033ddb54
seong-nyang <canyu1024@gmail.com>
2025-05-25 07:50:25 +0900

    Add files via upload

--- darkmode.js
+++ darkmode.js
@@ -1,7 +1,11 @@
 const darkToggle = document.getElementById("darkmode-toggle");
+const thumb = darkToggle.querySelector(".toggle-thumb");
 
 darkToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-  const thumb = darkToggle.querySelector(".toggle-thumb");
-  thumb.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+  const isDark = document.body.classList.contains("dark");
+
+  thumb.textContent = isDark ? "☀️" : "🌙";
+
+  darkToggle.classList.toggle("active", isDark);
 });
